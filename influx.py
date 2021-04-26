5480f1769a57cdd46d08d2f9e4883747507df6c7
Mattias Hemmingsson <mattias.hemmingsson@booli.se>
2021-04-26 21:41:08 +0200

    Working

--- influx.py
+++ influx.py
@@ -1,24 +1,28 @@
 from influxdb import InfluxDBClient
 import os
+import json
 #Inefluxdb settings
-client = InfluxDBClient(os.environ['INDB'],os.environ['INDB_PORT'] , os.environ['INDB_USER'], os.environ['INDB_PASSWORD'], os.environ['INDB_DATABASE'])
-#client.create_database(os.environ['INDB_DATABASE'])
+
+#client = InfluxDBClient(,os.environ['INDB_PORT'] , os.environ['INDB_USER'], os.environ['INDB_PASSWORD'], os.environ['INDB_DATABASE'])
+from influxdb_client import InfluxDBClient
+client = InfluxDBClient(url="http://influxdb:9999", token="yduilsJKTS576576ASD", org="ollebo")
 
 
 
 
 def add_influxdb(data):
     #Adding data to influx
-    json_data = [
-           {
-             "measurement": data['type'],
-             "tags": {
-                      "host": data['from'],
-                      "client": data['client']
-                      },
-             "time": data['timestamp'],
-             "fields": data['data']
-                   }
-                 ]
+    write_api = client.write_api()
+    #print(data)
+    dataToWrite = {"measurement": 
+        "h2o_feet", 
+        "tags": {
+          "location": "coyote_creek"
+          }, 
+        "fields": data, 
+          }
+    print(dataToWrite)
+    write_api.write("iot", "Ollebo", [dataToWrite])
 
-    client.write_points(json_data)
+
+
