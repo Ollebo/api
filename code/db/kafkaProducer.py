9e011451ea63131eaa4befa9e0d91b1d5cf3d7ec
Mattias Hemmingsson <mattias.hemmingsson@booli.se>
2023-11-19 15:52:54 +0100

    updated kafka brokers

--- code/db/kafkaProducer.py
+++ code/db/kafkaProducer.py
@@ -7,8 +7,6 @@ kafka_servers=str(os.environ.get('KAFKA', "['redpanda:9092']"))
 print("Connect to kafka servers: ", kafka_servers)
 
 producer = KafkaProducer(bootstrap_servers=kafka_servers,
-                             security_protocol='SSL',
-                             ssl_check_hostname='False',
                          value_serializer=lambda x: 
                          dumps(x).encode('utf-8'))
 def addToKafka(data):
