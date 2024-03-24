cb01d4ffdee61f2566ba974839d475870f6f2370
Mattias Hemmingsson <mattias.hemmingsson@booli.se>
2024-03-24 09:37:21 +0100

    Moving to aws

--- dist/run_in_docker.sh
+++
@@ -1,7 +0,0 @@
-docker run \
-    -v "$HOME"/.config/gcloud:/root/.config/gcloud \
-    -v /home/mattias/projects/ollebo/api:/workspace \
-    gcr.io/kaniko-project/executor:latest \
-    --dockerfile /workspace/Dockerfile \
-    --destination "docker.io/ollebo/api" \
-    --context dir:///workspace/ --no-push 
